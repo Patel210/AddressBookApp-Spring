7dd925a8f18a8002eff8d5fbd4d3286c2c55b312
Patel210 <pranavpatel2101997@gmail.com>
2020-12-14 17:43:38 +0530

    Added: Custom Exception Handler to return user friendly response in case validation fails

--- src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 public @Data class ContactDTO {
 	
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Person Name Invalid")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}([\\s][A-Z]{1}[a-z]{2,})?$", message = "Person Name Invalid")
 	public String name;
 	
 	@Pattern(regexp = "^[A-Za-z0-9,\\.]{3,}([\\s][A-Za-z0-9,\\.]{3,}){0,}$", message = "Person Address Invalid")
